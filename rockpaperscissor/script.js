7224b4495f885a97bb4189e5aabdb1736c947514
Dux <kartikmadaan1907@gmail.com>
2022-11-19 18:47:28 +0530

    Shortened the java code

--- rockpaperscissor/script.js
+++ rockpaperscissor/script.js
@@ -1,111 +1,44 @@
-let rock = document.getElementById('rock');
-let paper = document.getElementById('paper');
-let scissor = document.getElementById('scissor');
-let rocktext = rock.innerText;
-  let papertext = paper.innerText;
-  let scissortext = scissor.innerText;
-  let arr = [rocktext,papertext,scissortext]
- 
-const random = (arr) => {
-  let k = Math.floor(Math.random() * arr.length)
-  return arr[k]
+let squares = document.querySelectorAll('.name')
+
+  const hand = (randomm,square) =>{
+  if(randomm == square)
+    return 0;
+  else if((randomm.id == 'paper' && square.id == 'rock' )||(randomm.id == 'scissor') && (square.id == 'paper') ||(randomm.id == 'rock' && square.id == 'scissor'))
+    return -1;
+  return 1;
 }
-const calculaterock = () =>{
-  let randomtext = random(arr)
- if(randomtext == rocktext){
-   let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${rocktext} and computer's  choice is ${randomtext}`
-     let a2 = document.getElementById('result');
-   a2.innerText = 'Its a draw!'
- }
-  else if(randomtext == papertext){
-    let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${rocktext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You lose!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-    a4 -=1
-    a3.innerText = a4
-  }
- else{
-    let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${rocktext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You won!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-    a4 +=1
-    a3.innerText = a4
- }
+const random = (squares) => {
+  let k = Math.floor(Math.random() * squares.length)
+  return squares[k]
 }
-const calculatepaper = () => {
-  let randomtext = random(arr)
- if(randomtext == rocktext){
-    let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${papertext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You won!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-   console.log(a4 + 1)
-    a4 +=1
-    a3.innerText = a4
-  }
-  else if(randomtext == papertext){
-    let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${papertext} and computer's  choice is ${randomtext}`
-     let a2 = document.getElementById('result');
-   a2.innerText = 'Its a draw!'
- 
-  }
- else{
-    
-   let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${papertext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You lose!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-   console.log(a4 - 1)
-    a4 -=1
-    a3.innerText = a4
- }
+
+squares.forEach(square =>{
+  square.onclick = () => {
+ let randomm =  random(squares)
+//     console.log(randomm)
+   let score = hand(randomm,square)
+   let scoree = document.getElementById('score')
+   let k = Number(scoree.innerText)
+   k += score
+  scoree.innerText = k
+  let handd = document.getElementById('hand')
+    let message =  `your choice is ${square.id}${square.innerText} and computer's choice is ${randomm.id}${randomm.innerText}`
+    // let message =  " your choice is " + square.id +" " +square.innerText+" and computer's choice is "+randomm.id + " " + randomm.innerText + "."
+  handd.innerText = message
+  let result = document.getElementById('result')
+  if(score == 0)
+  result.innerText = 'The match is drawn!'
+  else if(score == 1)
+  result.innerText = 'You won the match!'
+  else
+  result.innerText = 'You lost the match!'
 }
-const calculatescissor = () =>{
-  let randomtext = random(arr)
- if(randomtext == rocktext){
-   let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${scissortext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You lose!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-    a4 -=1
-    a3.innerText = a4
- }
-  else if(randomtext == papertext){
-     let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${scissortext} and computer's  choice is ${randomtext}`
-      let a2 = document.getElementById('result');
-   a2.innerText = 'You won!'
-    let a3 = document.getElementById('score');
-    let a4 = Number(a3.innerText)
-    a4 +=1
-    a3.innerText = a4
-  }
- else{
-   let a1 = document.getElementById('hand');
-   a1.innerText = `Your choice is ${scissortext} and computer's choice is ${randomtext}`
-     let a2 = document.getElementById('result');
-   a2.innerText = 'Its a draw!'
-}
-}
+})
 const reset = () =>{
   let a1 = document.getElementById('hand');
    a1.innerText = ''
       let a2 = document.getElementById('result');
    a2.innerText = ''
     let a3 = document.getElementById('score');
-    a3.innerText = 0
+    a3.innerText = ''
 }
